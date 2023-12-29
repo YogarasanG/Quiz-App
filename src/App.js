11150ee8759236b7f4508ee14178ba5991f6616c
Yogarasan <yogarasanmani15@gmail.com>
2023-12-29 16:27:28 +0530

    myProject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Quiz from './components/Quiz';
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <div className="Header">
+        <h2>Quiz App</h2>
+      </div>
+      <div className="container">
+      <Quiz/>
+      </div>
+      <div className="footer">
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Questions from "./Questions";
+import './quiz.css'
+
+function Quiz() {
+  const [questions, setQuestions] = useState(0);
+  const [getScore, setGetScore] = useState(0);
+  const [score, setScore] = useState(false);
+  const cQues = Questions[questions];
+ 
+
+  const scoreFun = (selectedOptionIndex) => {
+    if (cQues.answer === selectedOptionIndex) {
+      setGetScore(getScore + 1);
+    }
+    const nextQuestion = questions + 1;
+    if (nextQuestion < Questions.length) {
+      setQuestions(questions + 1);
+    } else {
+      setScore(true);
+    }
+  };
+  const handleRestartQuiz = () => {
+    setQuestions(0);
+    setGetScore(0);
+    setScore(false);
+  };
+
+  return (
+    <div className="container">
+      <div className="quizApp">
+        {score ? (
+          <div className="score">
+          <h2>Your Total Score <span style={{textDecoration:'underline'}}>{getScore}</span><span style={{color:'#9E9FA5'}}>  Out Of {Questions.length}</span></h2> <br />
+          <button onClick={handleRestartQuiz}> restart Quiz</button>
+          </div>
+        ) : (
+          <>
+          
+            <div className="quizQuestion">
+              <h2> {cQues.question}</h2>
+            </div>
+            <div className="quizOptions">
+              {cQues.options.map((option, idx) => {
+                return (
+                  <ul>
+                    <li key={option.question} onClick={() => scoreFun(idx)}>{option}</li>
+                  </ul>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    
+    
+    </div>
+  );
+}
+
+export default Quiz;
