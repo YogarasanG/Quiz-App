40e9604ab79ad5acdbc68d31b37b55fcc682ffc8
Yogarasan <yogarasanmani15@gmail.com>
2024-01-01 17:12:27 +0530

    quiz

--- src/App.js
+++ src/App.js
@@ -2,14 +2,118 @@ import './App.css';
 import Quiz from './components/Quiz';
 import Footer from './components/Footer'
 
+
 function App() {
-  return (
+  const  quizQuestions = [
+    {
+        question: 'What is JSX in React?',
+        options: ['JavaScript XML', 'Java Syntax Extension', 'JSON XML', 'JavaScript Syntax Extension'],
+        answer: 0,
+      },
+      {
+        question: 'What is the purpose of the render() method in React?',
+        options: ['To perform side effects', 'To render HTML elements', 'To handle events', 'To define styles'],
+        answer: 1,
+      },
+      {
+        question: 'What is state in React?',
+        options: ['A built-in React component', 'An immutable data structure', 'A JavaScript object to store component data', 'A function to update props'],
+        answer: 2,
+      },
+      {
+        question: 'How can you pass data from a parent component to a child component in React?',
+        options: ['Using state', 'Using refs', 'Using props', 'Using context'],
+        answer: 2,
+      },
+      {
+        question: 'What is the purpose of the useEffect hook in React?',
+        options: ['To handle user input', 'To perform data fetching', 'To define component styles', 'To render JSX'],
+        answer: 1,
+      },
+      {
+        question: 'What is the virtual DOM in React?',
+        options: ['A representation of the browser\'s DOM in memory', 'A separate DOM for virtual reality applications', 'A lightweight version of the DOM', 'A component\'s initial state'],
+        answer: 0,
+      },
+      {
+        question: 'Explain the concept of props in React.',
+        options: ['Props are used to declare CSS styles for components', 'Props are short for "properties" and are used for passing data between components', 'Props are reserved keywords in React for component rendering', 'Props are used for defining component state'],
+        answer: 1,
+      },
+      {
+        question: 'How does React Router help in navigation in a React application?',
+        options: ['It provides a way to connect to a database', 'It enables server-side rendering', 'It manages the application state', 'It allows for declarative navigation between views'],
+        answer: 3,
+      },
+      {
+        question: 'What is the purpose of the useCallback hook in React?',
+        options: ['To memoize functions for optimization', 'To handle asynchronous operations', 'To define component lifecycle methods', 'To create reusable custom hooks'],
+        answer: 0,
+      },
+      {
+        question: 'What is the role of the shouldComponentUpdate lifecycle method in React?',
+        options: ['To create a new component instance', 'To determine if a component should re-render', 'To update the component state', 'To handle component mounting'],
+        answer: 1,
+      },
+      {
+        question: 'Explain the concept of lifting state up in React.',
+        options: ['Elevating the state of a child component to its parent component', 'Moving the state to a global state management system', 'Rendering components at a higher level in the component tree', 'Using state only in leaf components'],
+        answer: 0,
+      },
+      {
+        question: 'What is the purpose of the key prop in React lists?',
+        options: ['To define the order of elements in the list', 'To specify the color of list items', 'To uniquely identify each element in a list', 'To handle click events on list items'],
+        answer: 2,
+      },
+      {
+        question: 'How does React handle conditional rendering?',
+        options: ['By using the "if" statement directly in JSX', 'By utilizing the ternary operator in JSX', 'By relying on JavaScript "switch" statements', 'By creating separate components for each condition'],
+        answer: 1,
+      },
+      {
+        question: 'What is the purpose of the useRef hook in React?',
+        options: ['To create references to DOM elements', 'To manage component state', 'To define context providers', 'To handle form submissions'],
+        answer: 0,
+      },
+      {
+        question: 'Explain the concept of higher-order components (HOCs) in React.',
+        options: ['Components that render above the fold', 'Functions that return new components', 'Components with a higher z-index', 'Components that are part of the core React library'],
+        answer: 1,
+      },
+      {
+        question: 'How can you iterate over an array and render components for each item in React?',
+        options: ['Using the map method', 'Using the forEach method', 'Using a for loop', 'Using recursion'],
+        answer: 0,
+      },
+      {
+        question: 'What is the purpose of the "key" prop when mapping over an array of components in React?',
+        options: ['To set the background color of each component', 'To uniquely identify each rendered component', 'To define the order of rendering', 'To handle click events on the components'],
+        answer: 1,
+      },
+      {
+        question: 'Explain the concept of conditional rendering within a loop in React.',
+        options: ['Rendering components based on an arbitrary condition', 'Rendering a component only if a certain index is met', 'Rendering components in a loop is not possible', 'Rendering components in reverse order'],
+        answer: 0,
+      },
+      {
+        question: 'How can you achieve a dynamic number of iterations in a loop in React?',
+        options: ['By using the for loop with a dynamic condition', 'By setting the loop count in the component state', 'By using the loop counter as a prop', 'By relying on the useEffect hook for iterations'],
+        answer: 1,
+      },
+      {
+        question: 'What is the purpose of the React Fragment when rendering a list of components?',
+        options: ['To create a new component instance', 'To avoid the need for a parent div in the component tree', 'To add additional styling to each component', 'To handle state changes in the loop'],
+        answer: 1,
+      },
+   
+];
+return (
     <div className="App">      
       <div className="Header">
         <h2>Quiz App</h2>
       </div>
       <div className="container">
-      <Quiz/>
+      <Quiz Questions={quizQuestions}/>
       </div>
       <div className="footer">
         <Footer/>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import Questions from "./Questions";
 import './quiz.css'
 
-function Quiz() {
+function Quiz({Questions}) {
   const [questions, setQuestions] = useState(0);
   const [getScore, setGetScore] = useState(0);
   const [score, setScore] = useState(false);
   const cQues = Questions[questions];
  
-
   const scoreFun = (selectedOptionIndex) => {
     if (cQues.answer === selectedOptionIndex) {
       setGetScore(getScore + 1);
@@ -27,31 +25,32 @@ function Quiz() {
   };
 
   return (
-    <div className="container">
+    <div className="container">    
       <div className="quizApp">
-        {score ? (
-          <div className="score">
-          <h2>Your Total Score <span style={{textDecoration:'underline'}}>{getScore}</span><span style={{color:'#9E9FA5'}}>  Out Of {Questions.length}</span></h2> <br />
-          <button onClick={handleRestartQuiz}> restart Quiz</button>
+      {score ? (
+        <div className="score">
+        <h2>Your Total Score <span style={{textDecoration:'underline'}}>{getScore}</span><span style={{color:'#9E9FA5'}}>  Out Of {Questions.length}</span></h2> <br />
+        <button onClick={handleRestartQuiz}> restart Quiz</button>
+        </div>
+      ) : (
+        <>
+        
+          <div className="quizQuestion">
+            <h2> {cQues.question}</h2>
           </div>
-        ) : (
-          <>
-          
-            <div className="quizQuestion">
-              <h2> {cQues.question}</h2>
-            </div>
-            <div className="quizOptions">
-              {cQues.options.map((option, idx) => {
-                return (
-                  <ul>
-                    <li key={option.question} onClick={() => scoreFun(idx)}>{option}</li>
-                  </ul>
-                );
-              })}
-            </div>
-          </>
-        )}
-      </div>
+          <div className="quizOptions">
+            {cQues.options.map((option, idx) => {
+              return (
+                <ul>
+                 <li key={idx} onClick={() => scoreFun(idx)}>{option}</li>
+                </ul>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+     
     
     
     </div>
